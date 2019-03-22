fada786a03041f1eda6e851821f4b8f389c213f6
zhangyx96 <zhangyx310@gmail.com>
2019-03-22 21:37:06 +0800

    the basic floyd

--- Data.py
+++ Data.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def LoadData(car_path,road_path,cross_path):
-    car_info = np.loadtxt(car_path,dtype=str)
-    road_info = np.loadtxt(road_path,dtype=str)
-    cross_info = np.loadtxt(cross_path,dtype=str)
+    car_info = np.loadtxt(car_path,dtype=str,delimiter = ', ')
+    road_info = np.loadtxt(road_path,dtype=str,delimiter = ', ')
+    cross_info = np.loadtxt(cross_path,dtype=str,delimiter = ', ')
     return car_info,road_info,cross_info
--- Run.py
+++ Run.py
@@ -1,5 +1,7 @@
 import numpy as np
+
 from Data import LoadData
+from Floyd import Floyd
 
 def GenerateGraph(road_info,cross_info):  #初始化图
     road_nums =  road_info.shape[0]
@@ -7,19 +9,18 @@ def GenerateGraph(road_info,cross_info):  #初始化图
     MAXWEIGHT = 99999  #用来表示不存在路径
     graph = np.zeros([cross_nums,cross_nums],dtype = float) + MAXWEIGHT #初始化图
     for i in range(road_nums):
-        start_id = road_info[i,4] - 1  #cross编号从1开始
-        end_id = road_info[i,5] - 1
-        graph[start_id,end_id] = road_info[1]/road_info[2] #路径长度/最高限速
+        start_id = int(road_info[i,4])-1  #cross编号从1开始
+        end_id = int(road_info[i,5])-1
+        graph[start_id,end_id] = float(road_info[i,1])/float(road_info[i,2]) #路径长度/最高限速
     for j in range(cross_nums): #对角线置为0
         graph[j,j] = 0
-
     return graph
-
-
 
 def run(car_path,road_path,cross_path,answer_path):
     car_info,road_info,cross_info = LoadData(car_path,road_path,cross_path)
-    graph = GenerateGraph(road_info，cross_info)
+    graph_init = GenerateGraph(road_info,cross_info)
+    graph, path =  Floyd(graph_init)
+    a = 0
 
 
     
