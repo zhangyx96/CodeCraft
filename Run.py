13e458d23511459fa827fc420d1f9ef6620d8242
zhangyx96 <zhangyx310@gmail.com>
2019-03-22 21:15:36 +0800

    generate the graph

--- Run.py
+++ Run.py
@@ -1,15 +1,25 @@
 import numpy as np
 from Data import LoadData
 
-def GenerateGraph(cross_info):
-    graph = np.zeros([cross_info.shape[0],cross_info.shape[0]],dtype = float)
+def GenerateGraph(road_info,cross_info):  #初始化图
+    road_nums =  road_info.shape[0]
+    cross_nums = cross_info.shape[0]
+    MAXWEIGHT = 99999  #用来表示不存在路径
+    graph = np.zeros([cross_nums,cross_nums],dtype = float) + MAXWEIGHT #初始化图
+    for i in range(road_nums):
+        start_id = road_info[i,4] - 1  #cross编号从1开始
+        end_id = road_info[i,5] - 1
+        graph[start_id,end_id] = road_info[1]/road_info[2] #路径长度/最高限速
+    for j in range(cross_nums): #对角线置为0
+        graph[j,j] = 0
+
     return graph
 
 
 
 def run(car_path,road_path,cross_path,answer_path):
     car_info,road_info,cross_info = LoadData(car_path,road_path,cross_path)
-    graph = GenerateGraph(road_info)
+    graph = GenerateGraph(road_info，cross_info)
 
 
     
